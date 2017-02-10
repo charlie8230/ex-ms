412ebd8415a97b99e80e80aa16d6e9d27bbf3342
carlos <carlos@moranjr.com>
2017-02-10 17:13:27 -0500

    app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 
 let app = {};
 
+/*
+
+  Needs Plugin system - requires conventions be followed returns chains
+  Needs Singleton Service
+  Needs States
+  Needs stacks
+  Needs Public API
+  Needs Modules
+  Needs config
+  Needs data-*
+  Allows Views (how does data flow?)
+  Allows Streams
+  Allows delegation
+  Enable custom build
+  Allows composition
+
+*/
 
 
 module.exports = app;
