dd3213f3d63841b72e3c693661cbb8b647f5fec7
carlos <carlos@moranjr.com>
2017-02-21 23:44:29 -0500

    store

--- src/state.js
+++ src/state.js
@@ -13,13 +13,47 @@ const s = {
 // 
 const ACTIONS = {
   SET_VAL: 'SET_VAL',
+  REGISTER_ITEM: 'REGISTER_ITEM',
   GET_VAL: 'GET_VAL',
   GET_ALL: 'GET_ALL'
 };
 
+function registerItem(name, fn) {
+  return {
+    type: ACTIONS.REGISTER_ITEM,
+    name,
+    fn
+  }
+}
+
+function item(state={},action){
+  switch (action.type) {
+    case ACTIONS.REGISTER_ITEM:
+      return {
+        name: action.name,
+        fn: action.fn
+      }
+    default:
+      return state;
+  }
+}
 // reducer
+function items(state=[], action) {
+  switch(action.type) {
+    case ACTIONS.REGISTER_ITEM:
+      return [...state, item(undefined, action)]
+    default:
+      return state;
+  }
+}
+
 // store
+const reducers = combineReducers(items);
+const store = createStore(reducers);
+//unsub
+
 // subscribe
+
 // dispatch
 
 //  API
@@ -29,8 +63,6 @@ let defaultState = {
     debug: true
   } // immutable ?
 }
-
-window.immutable = immutable;
 
 
 module.exports = class AppState {
