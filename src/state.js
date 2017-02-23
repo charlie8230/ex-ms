ac4d1934526c2843c957d513e168ea3e635acf80
carlos <carlos@moranjr.com>
2017-02-23 01:28:57 -0500

    register modules

--- src/state.js
+++ src/state.js
@@ -2,10 +2,10 @@ let { createStore, combineReducers } = require('redux');
 
 //  let immutable = require('immutable');
 //  stacks
-
-// initial state
+//  register different types
+//  initial state
 const s = {
-  services: [], // DOM, AJAX, PUBSUB << Interact with Redux? >> Rx Streams>>? Templating (plugin)>>
+  services: [], // DOM, DOM Events, App Services, AJAX, PUBSUB << Interact with Redux? >> Rx Streams>>? Templating (plugin)>>
   modules: [],
   plugins: [],
   globalConfig: {}
@@ -14,15 +14,25 @@ const s = {
 const ACTIONS = {
   SET_VAL: 'SET_VAL',
   REGISTER_ITEM: 'REGISTER_ITEM',
+  REGISTER_MODULE: 'REGISTER_MODULE',
   GET_VAL: 'GET_VAL',
-  GET_ALL: 'GET_ALL'
+  GET_ALL: 'GET_ALL',
+  IS_MODULE: 'IS_MODULE'
 };
 
-function registerItem(name, fn) {
+function registerItem(name, fn, itemType) {
   return {
     type: ACTIONS.REGISTER_ITEM,
+    itemType,
     name,
     fn
+  }
+}
+
+function setConfig(configType,value) {
+  return {
+    type: configType,
+    value
   }
 }
 
@@ -47,8 +57,29 @@ function items(state=[], action) {
   }
 }
 
+function stack(state={services:[],modules:[],plugins:[]}, action) {
+  console.log('stack', action);
+  switch(action.itemType) {
+    case ACTIONS.IS_MODULE:
+      return Object.assign({}, state,{
+        modules: [...(state.modules||[]),item(undefined, action)]
+      });
+    default:
+      return state;
+  }
+}
+
+function globalConfig(state={}, action) {
+  switch(action.type) {
+    case 'GLOBAL_CONFIG':
+      return Object.assign({}, state, action.value);
+    default:
+      return state;
+  }
+}
+
 // store
-const reducers = combineReducers({items});
+const reducers = combineReducers({stack, globalConfig});
 const store = createStore(reducers);
 //handler
 function handleSubscribe() {
@@ -56,7 +87,9 @@ function handleSubscribe() {
 }
 let unsubscribe = store.subscribe(handleSubscribe);
 // dispatch
+store.dispatch(registerItem('name',function(){console.log('I am here')},'IS_MODULE'))
 store.dispatch(registerItem('name',function(){console.log('I am here')}))
+store.dispatch(setConfig('GLOBAL_CONFIG',{debug:true}));
 
 //  API
 
